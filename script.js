52e650a00bd534613a9ec6de7f90d56dfb62cfd1
Subhransu-sekhar-1355 <sekhar1355@gmail.com>
2025-02-14 15:41:36 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// scripts.js
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
+
+document.getElementById('contact-button').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'contact.html';
+});
